bc749180c3052433e7ff6febe8e8416f4c70d657
Cesar1986BR <cesar_ags@outlook.com>
2017-11-14 13:27:59 -0200

    xcvcxv

---
+++ SistemaControle/Controllers/MVC/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SistemaControle.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Sistema Alunos Beta";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Entre em contato";
+
+            return View();
+        }
+    }
+}
---
+++ SistemaControle/Global.asax.cs
@@ -0,0 +1,32 @@
+﻿using SistemaControle.Classes;
+using System.Data.Entity;
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace SistemaControle
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            Database.SetInitializer( new  MigrateDatabaseToLatestVersion<Models.ControleContext,Migrations.Configuration>());
+            this.CheckRole();
+            AreaRegistration.RegisterAllAreas();
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+
+        private void CheckRole()
+        {
+
+            Ultilidades.CheckRole("Admin");
+            Ultilidades.CheckRole("Estudante");
+            Ultilidades.CheckRole("Professor");
+
+        }
+    }
+}
